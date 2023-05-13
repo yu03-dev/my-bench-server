79e63a290289257460b6e470af0cd993e3f31a3f
yuto <yuto.0313king@icloud.com>
2023-05-14 03:49:49 +0900

    fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser())
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:8080",
+  origin: process.env.BASE_URL || "http://localhost:8080",
   credentials: true
 }));
 
