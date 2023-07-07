9299caae961046b2c7dc8838f1d415eb7837a812
yuto <yuto.0313king@icloud.com>
2023-07-07 22:21:52 +0900

    🎨 prettierによる整形

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const createError = require('http-errors');
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const createError = require("http-errors");
+const cors = require("cors");
 
-app.use(cors({
-  origin: process.env.BASE_URL || "http://localhost:4173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.BASE_URL || "http://localhost:8080", //4173
+    credentials: true,
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
 // authorization
 require("./config/passport")(app);
 
 // routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/mypage', require('./routes/mypage'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/mypage", require("./routes/mypage"));
 // app.use('/api/history', require('./routes/history'))
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // send error response as JSON
   res.status(err.status || 500);
@@ -36,11 +38,10 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(port, () => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log(`App listening on ${port}`)
-    console.log('NODE_ENV:', process.env.NODE_ENV)
+  if (process.env.NODE_ENV === "development") {
+    console.log(`App listening on ${port}`);
+    console.log("NODE_ENV:", process.env.NODE_ENV);
   }
-})
+});
 
 module.exports = app;
-
