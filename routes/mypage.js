3c8b76afeb11d6169878cb44c5f73d2ac99c64b9
yuto <yuto.0313king@icloud.com>
2023-07-08 13:52:09 +0900

    ♻️ レコードのレスポンスを配列形式に変更

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,193 +1,192 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../models')
-const Record = db.Record
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const Record = db.Record;
 
-
-const getRecord = async(user) => {
+const getRecord = async (user) => {
   const records = await Record.findAll({
     where: {
-      userId: user.id
+      userId: user.id,
     },
-    order: [['createdAt', 'DESC']]
+    order: [["createdAt", "DESC"]],
   });
 
   return records.reduce((result, record) => {
-    const date = record.createdAt.toISOString().slice(0, 10)
-      if (!result[date]) {
-        result[date] = []
-      }
-      result[date].push(record)
-      return result
-  }, {});
-}
+    const date = record.createdAt.toISOString().slice(0, 10);
+    if (result.length === 0 || result[result.length - 1].date != date) {
+      result.push({
+        date: date,
+        data: [],
+      });
+    }
+    result[result.length - 1].data.push(record);
+    return result;
+  }, []);
+};
 
-router.get('/', async function(req, res) {
-  const isAuth = req.isAuthenticated()
+router.get("/", async function (req, res) {
+  const isAuth = req.isAuthenticated();
   if (isAuth) {
-    const user = req.user
+    const user = req.user;
     try {
-      const groupedRecords = await getRecord(user)
+      const groupedRecords = await getRecord(user);
       res.json({
         success: true,
-        message: 'レコードの取得に成功しました',
+        message: "レコードの取得に成功しました",
         user: user,
         records: groupedRecords,
         error: null,
-        isAuth: isAuth
+        isAuth: isAuth,
       });
     } catch (err) {
-      console.error(err)
+      console.error(err);
       res.json({
         success: false,
-        message: 'レコードの取得に失敗しました',
+        message: "レコードの取得に失敗しました",
         user: user,
         records: null,
         error: err,
-        isAuth: isAuth
+        isAuth: isAuth,
       });
     }
-
   } else {
     res.json({
       success: false,
-      message: 'データを取得できませんでした',
+      message: "データを取得できませんでした",
       user: null,
       records: null,
       error: null,
-      isAuth: isAuth
+      isAuth: isAuth,
     });
   }
 });
 
-router.post('/', async function(req, res) {
-  const isAuth = req.isAuthenticated()
-  const weight = req.body.weight
-  const reps = req.body.reps
+router.post("/", async function (req, res) {
+  const isAuth = req.isAuthenticated();
+  const weight = req.body.weight;
+  const reps = req.body.reps;
 
   if (isAuth) {
-    const user = req.user
+    const user = req.user;
     const newRecord = {
       weight: weight,
       reps: reps,
-      userId: user.id
-    }
+      userId: user.id,
+    };
     try {
-      await Record.create(newRecord)
-      const groupedRecords = await getRecord(user)
+      await Record.create(newRecord);
+      const groupedRecords = await getRecord(user);
       res.json({
         success: true,
-        message: '正常にレコードを作成できました',
+        message: "正常にレコードを作成できました",
         user: user,
         records: groupedRecords,
         error: null,
-        isAuth: isAuth
+        isAuth: isAuth,
       });
-    } catch(err) {
-      console.error(err)
+    } catch (err) {
+      console.error(err);
       res.json({
         success: false,
-        message: 'レコードの作成に失敗しました',
+        message: "レコードの作成に失敗しました",
         user: user,
-        records: await getRecord(user) || null,
+        records: (await getRecord(user)) || null,
         error: null,
-        isAuth: isAuth
+        isAuth: isAuth,
       });
     }
   } else {
     res.json({
       success: false,
-      message: 'データを取得できませんでした',
+      message: "データを取得できませんでした",
       user: null,
       records: null,
       error: null,
-      isAuth: isAuth
+      isAuth: isAuth,
     });
   }
 });
 
-
-router.put('/:id', async function(req, res) {
-  const recordId = req.params.id
-  const isAuth = req.isAuthenticated()
-  const user = req.user
-  const record = await Record.findByPk(recordId)
+router.put("/:id", async function (req, res) {
+  const recordId = req.params.id;
+  const isAuth = req.isAuthenticated();
+  const user = req.user;
+  const record = await Record.findByPk(recordId);
 
   if (isAuth && user.id == record.userId) {
     try {
-      record.weight = req.body.weight
-      record.reps = req.body.reps
-      await record.save()
-      const groupedRecords = await getRecord(user)
+      record.weight = req.body.weight;
+      record.reps = req.body.reps;
+      await record.save();
+      const groupedRecords = await getRecord(user);
       res.json({
         success: true,
-        message: 'レコードの更新に成功しました',
+        message: "レコードの更新に成功しました",
         user: user,
         records: groupedRecords,
         error: null,
-        isAuth: isAuth
+        isAuth: isAuth,
       });
     } catch (err) {
       res.json({
         success: false,
-        message: 'レコードを更新できませんでした',
+        message: "レコードを更新できませんでした",
         user: user,
-        records: await getRecord(user) || null,
+        records: (await getRecord(user)) || null,
         error: err,
-        isAuth: isAuth
+        isAuth: isAuth,
       });
     }
   } else {
     res.json({
       success: false,
-      message: 'データの取得ができませんでした',
+      message: "データの取得ができませんでした",
       user: user,
       records: null,
       error: null,
-      isAuth: isAuth
+      isAuth: isAuth,
     });
   }
 });
 
-router.delete('/:id', async function(req, res) {
-  const recordId = req.params.id
-  const isAuth = req.isAuthenticated()
-  const user = req.user
-  const record = await Record.findByPk(recordId)
+router.delete("/:id", async function (req, res) {
+  const recordId = req.params.id;
+  const isAuth = req.isAuthenticated();
+  const user = req.user;
+  const record = await Record.findByPk(recordId);
 
   if (isAuth && user.id == record.userId) {
     try {
-      await record.destroy()
-      const groupedRecords = await getRecord(user)
+      await record.destroy();
+      const groupedRecords = await getRecord(user);
       res.json({
         success: true,
-        message: 'レコードの削除に成功しました',
+        message: "レコードの削除に成功しました",
         user: user,
         records: groupedRecords,
         error: null,
-        isAuth: isAuth
+        isAuth: isAuth,
       });
     } catch (err) {
       res.json({
         success: false,
-        message: 'レコードの削除に失敗しました',
+        message: "レコードの削除に失敗しました",
         user: user,
-        records: await getRecord(user) || null,
+        records: (await getRecord(user)) || null,
         error: err,
-        isAuth: isAuth
+        isAuth: isAuth,
       });
     }
   } else {
     res.json({
       success: false,
-      message: 'データを取得できませんでした',
+      message: "データを取得できませんでした",
       user: user,
       records: null,
       error: null,
-      isAuth: isAuth
+      isAuth: isAuth,
     });
   }
-
 });
 
 module.exports = router;
