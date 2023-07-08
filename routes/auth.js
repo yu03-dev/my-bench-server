66657f51f1a75e4d929b27838f89a510ccb86169
yuto <yuto.0313king@icloud.com>
2023-07-08 13:50:20 +0900

    ♻️ prettierによるコードの整形

--- routes/auth.js
+++ routes/auth.js
@@ -1,117 +1,115 @@
-const express = require('express')
-const router = express.Router()
-const passport = require("passport")
-const bcrypt = require("bcrypt")
-const db = require('../models')
-const User = db.User
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const db = require("../models");
+const User = db.User;
 
-
-router.post('/signin', passport.authenticate('local'), (req, res) => {
-  const user = req.user
+router.post("/signin", passport.authenticate("local"), (req, res) => {
+  const user = req.user;
   if (user) {
     res.json({
       success: true,
-      message: '正常にサインインしました',
+      message: "正常にサインインしました",
       user: user,
       records: null,
       error: null,
-      isAuth: true
+      isAuth: true,
     });
-  }
-  else {
+  } else {
     res.json({
       success: false,
-      message: '正常にサインインできませんでした',
+      message: "正常にサインインできませんでした",
       user: user,
       records: null,
       error: null,
-      isAuth: false
+      isAuth: false,
     });
   }
 });
 
-router.post('/signout', function(req, res) {
+router.post("/signout", function (req, res) {
   req.logout((error) => {
     if (error) {
-      console.error(error)
-      rerturn
+      console.error(error);
+      rerturn;
     }
     res.json({
       success: true,
-      message: '正常にログアウトしました',
+      message: "正常にログアウトしました",
       user: null,
       records: null,
       error: null,
-      isAuth: false
+      isAuth: false,
     });
-  })
-})
+  });
+});
 
-router.post('/signup', async function(req, res) {
-  const username = req.body.username
-  const password = req.body.password
-  const repassword = req.body.repassword
+router.post("/signup", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const repassword = req.body.repassword;
 
   try {
     const user = await User.findOne({
-      where: {username: username}
-    })
+      where: { username: username },
+    });
     if (user) {
       res.json({
         success: false,
-        message: 'すでにユーザーが存在します',
+        message: "すでにユーザーが存在します",
         user: null,
         records: null,
         error: null,
-        isAuth: false
+        isAuth: false,
       });
     } else if (password == repassword) {
-      const hashedPassword = await bcrypt.hash(password, 10)
+      const hashedPassword = await bcrypt.hash(password, 10);
       try {
         const newUser = await User.create({
           username: username,
-          password: hashedPassword
-        })
+          password: hashedPassword,
+        });
         res.json({
           success: true,
-          message: '正常にサインアップできました',
+          message: "正常にサインアップできました",
           user: null,
           records: null,
           error: null,
-          isAuth: false
+          isAuth: false,
         });
-      } catch(error) {
-        console.error(err)
+      } catch (error) {
+        console.error(err);
         res.json({
           success: false,
-          message: 'ユーザーの作成に失敗しました',
+          message: "ユーザーの作成に失敗しました",
           user: null,
           records: null,
           error: err,
-          isAuth: false
+          isAuth: false,
         });
       }
     } else {
       res.json({
         success: false,
-        message: 'パスワードが一致しませんでした',
+        message: "パスワードが一致しませんでした",
         user: null,
         records: null,
         error: null,
-        isAuth: false
+        isAuth: false,
       });
     }
-  } catch(error) {
+  } catch (error) {
     console.error(err);
     res.json({
       success: false,
-      message: 'ユーザーの検索に失敗しました',
+      message: "ユーザーの検索に失敗しました",
       user: null,
       records: null,
       error: null,
-      isAuth: false
+      isAuth: false,
     });
   }
-})
+});
 
-  module.exports = router;
+module.exports = router;
